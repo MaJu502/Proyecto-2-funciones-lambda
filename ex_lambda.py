4a6181c1f4c4bf84464a9b7e3b4dd894e67bf0bd
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-11-13 17:27:22 -0600

    initial

---
+++ ex_lambda.py
@@ -0,0 +1,11 @@
+"""
+Universidad del valle de Guatemala
+Teoría de la computación 
+13/11/2022
+@author Marco Jurado 20308
+
+Expresiones utilizando funciones lambda para determinar
+la diferencia de las mismas usando beta y alpha.
+"""
+
+
